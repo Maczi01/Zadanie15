1cbf86032d6734de579263af84245b183d14a5a1
Maczi01 <system11>
2019-02-01 20:03:03 +0100

    Initial commit

---
+++ src/ArrayTest.java
@@ -0,0 +1,12 @@
+public class ArrayTest {
+
+    public static void main(String[] args) {
+
+        int[] tablicaPierwsza = {1,2,3,4};
+        int[] tablicaDruga = {1,2,3,5,};
+
+        System.out.println(ArrayComparator.compare(tablicaPierwsza,tablicaDruga));
+
+    }
+
+}
